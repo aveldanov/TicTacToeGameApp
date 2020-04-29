1ddf62a0374068f32b45673583af154d6f02a0d9
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-29 15:27:40 -0700

    iOS

--- TicTacToeGameApp/ViewController.swift
+++ TicTacToeGameApp/ViewController.swift
@@ -9,32 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   // 1 noughts, 2 is crosses
   
   var  activePlayer = 1
+  
+  var  gameState = [0,0,0,0,0,0,0,0,0] // 0 -none, 1 - nought, 2 - cross
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
-
+  
+  
   @IBAction func buttonPressed(_ sender: UIButton) {
     
-    if activePlayer == 1{
-       sender.setImage(UIImage(named: "nought.png"), for: .normal)
-      activePlayer = 2
-    }else{
-        sender.setImage(UIImage(named: "cross.png"), for: .normal)
+    let activePosition = sender.tag - 1
+    
+    if gameState[activePosition] == 0{
+      gameState[activePosition] = activePlayer
       
-      activePlayer = 1
+      if activePlayer == 1{
+        sender.setImage(UIImage(named: "nought.png"), for: .normal)
+        
+        
+        
+        activePlayer = 2
+        
+      }else{
+        sender.setImage(UIImage(named: "cross.png"), for: .normal)
+
+        activePlayer = 1
+        
+      }
       
     }
     
+    print(gameState)
     
-  
+    
+    
+    
+    
+    
+    
+    
     
   }
 }
