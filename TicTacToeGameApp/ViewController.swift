515ca7b4dc32c9356af0775147560b85def567ce
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-29 14:51:26 -0700

    first commit

---
+++ TicTacToeGameApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToeGameApp
+//
+//  Created by Veldanov, Anton on 4/29/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
