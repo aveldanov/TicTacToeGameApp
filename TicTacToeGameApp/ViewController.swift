8791552aea5b1c12801a220e3843761867f3b0bc
Anton Veldanov <anton.veldanov@gmail.com>
2020-04-29 19:27:10 -0700

    iOS

--- TicTacToeGameApp/ViewController.swift
+++ TicTacToeGameApp/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  // 1 noughts, 2 is crosses
+  var activeGame = true
   
+  
+  // 1 noughts, 2 is crosses
   var  activePlayer = 1
   
   var  gameState = [0,0,0,0,0,0,0,0,0] // 0 -none, 1 - nought, 2 - cross
+  
+  let winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  
+  
+  
+  @IBOutlet weak var winnerLabel: UILabel!
+  
+  
   
   
   override func viewDidLoad() {
@@ -27,7 +37,7 @@ class ViewController: UIViewController {
     
     let activePosition = sender.tag - 1
     
-    if gameState[activePosition] == 0{
+    if gameState[activePosition] == 0 && activeGame{
       gameState[activePosition] = activePlayer
       
       if activePlayer == 1{
@@ -45,6 +55,29 @@ class ViewController: UIViewController {
       }
       
     }
+//    let winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+    for combination in winningCombinations{
+//      print(gameState)
+//      print("comb0", combination[0])
+//      print("comb1", combination[1])
+//      print("comb2", combination[2])
+//      
+//      print("GScomb0", gameState[combination[0]])
+//      print("GScomb1", gameState[combination[1]])
+//      print("GScomb2", gameState[combination[2]])
+//      
+//      print("")
+
+      if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[0]] == gameState[combination[2]]{
+
+        
+        activeGame = false
+        print("We have a winner \(gameState[combination[0]])")
+
+      }
+      
+    }
     
     print(gameState)
     
@@ -57,5 +90,11 @@ class ViewController: UIViewController {
     
     
   }
+  
+  
+  
+  
+  
+  
 }
 
