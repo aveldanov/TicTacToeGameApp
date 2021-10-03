dd3edd62690487a81d0e49f66cc73ac0b2fbaf3d
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-03 11:55:35 -0700

    iOS

---
+++ README.md
@@ -0,0 +1,11 @@
+# TicTacToe2020
+### AppStore Available
+
+
+
+<p float="left">
+<img src="Images/TicTacToe2020.jpg" alt="drawing" width="200"/>
+</p>
+
+
+### Easy fun game everyone likes
---
+++ TicTacToeGameAppUITests/TicTacToeGameAppUITests.swift
@@ -0,0 +1,53 @@
+//
+//  TicTacToeGameAppUITests.swift
+//  TicTacToeGameAppUITests
+//
+//  Created by Veldanov, Anton on 9/1/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import XCTest
+
+class TicTacToeGameAppUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+
+    }
+    
+    
+    func testExample2() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+
+    }
+    
+    
+    func testExample3() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+
+    }
+
+
+}
